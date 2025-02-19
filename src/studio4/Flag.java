b1ba968ee60886499debeb2e847dca330c7721ef
raina <raina@localhost>
2025-02-19 15:43:36 -0600

    rose bud thorn

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,25 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 1);
+		StdDraw.setYscale(0, 1);
+		
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 175, 175);
+		StdDraw.filledRectangle(.5, .5, .3, .2);
+		StdDraw.setPenColor(196, 226, 232);
+		StdDraw.filledRectangle(.5, .5, .15, .15);
+		StdDraw.setPenColor(0, 0, 128);
+		StdDraw.filledRectangle(.5, .5, .1, .1);
+		StdDraw.setPenColor(240, 18, 141);
+		StdDraw.filledRectangle(.5, .5, .05, .05);
+		// StdDraw.filledRectangle(16, 3, 6, 3);
+		// StdDraw.filledRectangle(16, 13, 6, 3);
+		// StdDraw.setPenColor(0, 0, 127);
+		// StdDraw.filledRectangle(11, 8, 11, 1);
+		// StdDraw.filledRectangle(8, 8, 1, 8);
+		
+	
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,41 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		// rectangle 255 109 182 true 0.5 0.5 0.2 0.2
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+
+		
+		Color pretty = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(pretty);
+		
+		if (shapeType.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+		StdDraw.rectangle(x,y,halfWidth,halfHeight);
+		}
+		
+		
+		if (shapeType.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double[] x = new double[x1, x2, x3];
+			double[] y = new double[y1, y2, y3];
+		
+			StdDraw.polygon(x,y);
+
+
 		
 	}
 }
